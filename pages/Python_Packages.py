1ef2663b94e8e966932cbb2807e53ee03d0fa62d
Brad Penney <brad.penney@nscc.ca>
2023-12-07 18:56:36 -0400

    Added new page for python packages

---
+++ pages/Python_Packages.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Python Packages")
+
+st.markdown("The Python Package Index (PyPI) helps you find and install \
+             Python packages developed and shared by the Python community.")
